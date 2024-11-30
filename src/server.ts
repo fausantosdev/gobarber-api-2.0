0a78414b267816882e01efd6c82d022e93c9111f
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2024-11-29 23:07:42 -0300

    refactor: creates a separate file for routes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import express from 'express'
+import routes from './routes'
 
 const app = express()
 
-app.get('/', (request, response): any => {
-  return response.json({ message: 'Hello World' })
-})
+app.use(routes)
 
 app.listen(3333, () => {
   console.log(`~ server running on port ${3333}`)
