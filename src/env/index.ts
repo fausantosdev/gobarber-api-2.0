4ac9886695a27d72b5209506c4aa8f939a2ba05e
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2024-12-08 20:46:15 -0300

    chore: configures the loading of environment variables

---
+++ src/env/index.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  PORT: z.coerce.number().default(3333),
+  NODE_ENV: z
+    .enum(['development', 'test', 'production'])
+    .default('development'),
+  DBTYPE: z.enum(['postgres', 'mysql']).default('postgres'),
+  DBHOST: z.string(),
+  DBPORT: z.coerce.number().default(3333),
+  DBNAME: z.string(),
+  DBUSER: z.string(),
+  DBPASS: z.string(),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (!_env.success) {
+  throw new Error(`x invalid environment variables. ${_env.error.format()}`)
+}
+
+export const env = _env.data
