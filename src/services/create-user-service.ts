088a6b9c31057296c71e4a0ee97f24a411799777
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-01-12 17:01:10 -0300

    feat: created user creation service

---
+++ src/services/create-user-service.ts
@@ -0,0 +1,35 @@
+import { User } from '../db/entities/user'
+
+import { UserRepository } from '../db/repositories/user-repository'
+
+type Request = {
+  name: string
+  email: string
+  password: string
+}
+
+class CreateAppointmentService {
+  constructor(private usersRepository: UserRepository) {
+    this.usersRepository = usersRepository
+  }
+
+  public async execute({ name, email, password }: Request): Promise<User> {
+    const checkUserExists = await this.usersRepository.findOne({
+      where: { email },
+    })
+
+    if (checkUserExists) throw new Error('Email address already used')
+
+    const user = this.usersRepository.create({
+      name,
+      email,
+      password,
+    })
+
+    const newUser = this.usersRepository.save(user)
+
+    return newUser
+  }
+}
+
+export default CreateAppointmentService
