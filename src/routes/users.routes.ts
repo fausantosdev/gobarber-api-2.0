d6d89f3c2ed5c43de1201462a897858f907e72eb
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-01-12 17:06:41 -0300

    feat: integrated user creation service into route handler

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,21 @@
 import { Router } from 'express'
 
+import { UserRepository } from '../db/repositories/user-repository'
+import CreateUserService from '../services/create-user-service'
+
 const routes = Router()
 
+const usersRepository = new UserRepository()
+
 routes.post('/', async (request, response): any => {
+  const createUserService = new CreateUserService(usersRepository)
+
   try {
-    return response.send()
+    const { name, email, password } = request.body
+
+    const user = await createUserService.execute({ name, email, password })
+
+    return response.json(user)
   } catch (error: any) {
     return response.status(400).json({ error: error.message })
   }
