54d04f1c76918a4badcc9efbe0663c0d7e6fb228
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-01-12 22:30:44 -0300

    chore: add api secret key typing

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import { z } from 'zod'
 
 const envSchema = z.object({
+  APP_KEY: z.string(),
   PORT: z.coerce.number().default(3333),
   NODE_ENV: z
     .enum(['development', 'test', 'production'])
